e77d858d767a3747cc901c705e4604a78d140b29
RMHonor <me@r-honor.uk>
2016-11-22 14:40:47 +0000

    Minor modifications to phone-number component and added to cascade

--- src/app/common/form/phone-number/phone-number.component.ts
+++ src/app/common/form/phone-number/phone-number.component.ts
@@ -1,14 +1,16 @@
-import {Component, Input, SimpleChanges} from '@angular/core';
+import {Component, Input, SimpleChanges, Output} from '@angular/core';
 
 @Component({
     selector: 'phone-number',
     templateUrl: './phone-number.component.html'
 })
 export class PhoneNumberComponent {
+    label: string = "Please enter your phone number";
+    placeholder: string = "Phone number";
+    errorMsg: string = "Please enter a valid telephone number.";
+
     error: boolean = false;
-    regex = /^[0-9\s+]{9,13}$/;
-    @Input() name: string;
-    @Input() placeholder: string;
+    regex = /^[0-9\s+]{9,14}$/;
 
     validate(value: string){
         this.error = !this.regex.test(value);
@@ -16,12 +18,6 @@ export class PhoneNumberComponent {
             //show tick
         } else {
             //show cross
-        }
-    }
-
-    ngOnChanges(changes: SimpleChanges){
-        if (this.placeholder == undefined){
-            this.placeholder = this.name;
         }
     }
 }
